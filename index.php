e78ad8b9b3c047db1d7aa67b285f3a7f94af5b09
tyleradams11 <134800311+tyleradams11@users.noreply.github.com>
2023-06-04 13:55:17 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf-8">
+<title>My PHP Website</title>
+</head>
+<body>
+<h1>Welcome to the cloud!</h1>
+<form action = "" method = "post">
+Your name:
+<br>
+<input type = "text" name = "name" size = "30" maxlength = "30">
+<br>
+<input type = "submit" name = "submit" value = "Submit">
+</form>
+<?php
+if (isset($_POST['submit']))
+{
+$yourname = $_POST['name'];
+echo "<p>Hi, $yourname, welcome to my cloud.</p>";
+}
+?>
+</body>
+</html>
